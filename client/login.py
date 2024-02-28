8340952b0696f9af99801f6bb719987abbe9ebc9
thomas-cerruti <thomas.cerruti@laplateforme.io>
2024-02-28 17:10:06 +0100

    changement background gris, et centrage des boutons

--- client/login.py
+++ client/login.py
@@ -8,17 +8,17 @@ class Login(tk.Frame):
         # self.client = client
 
         master.geometry("420x520")
-        master.configure(bg=master.MAIN_COLOR)
+        self.configure(bg="#424549")
 
         self.create_widgets()
 
     def create_widgets(self):
-        email_label = tk.Label(self, text="Email")
-        email_label.pack(pady=10)
+        email_label = tk.Label(self, text="Email", bg="#424549", fg="white")
+        email_label.pack(pady=(150, 0))
         email_entry = tk.Entry(self)
         email_entry.pack(pady=10)
 
-        password_label = tk.Label(self, text="Password")
+        password_label = tk.Label(self, text="Password", bg="#424549", fg="white")
         password_label.pack(pady=10)
         password_entry = tk.Entry(self, show="*")
         password_entry.pack(pady=10)
